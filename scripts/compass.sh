7c0ba0a92bd53374b605d914b42a5d06c7f2ee52
neolao <contact@neolao.com>
2014-06-11 00:32:06 +0200

    Add a font

---
+++ scripts/compass.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+scriptPath="$( readlink -f "$( dirname "$0" )" )/$( basename "$0" )"
+currentDirectory=$(dirname $scriptPath)
+projectDirectory="$currentDirectory/../styles"
+
+#/var/lib/gems/1.9.0/bin/compass watch $projectDirectory
+compass watch $projectDirectory
